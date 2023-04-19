a46fba3c3d868d5d983947f7cf5f19a6274eae45
sathya <satyavardhanreddy12@gmail.com>
2023-04-19 12:44:35 +0530

    5th

---
+++ area.py
@@ -0,0 +1,9 @@
+
+b = int(input("Enter base : "))
+h = int(input("Enter height : "))
+print("area of triangle is ",(0.5*h*b))
+side = int(input("Enter side : "))
+print("Area of square is",side*side)
+l = int(input("Enter length : "))
+b = int(input("Enter breadth : "))
+print("Area of rectangle is : ",l*b)
